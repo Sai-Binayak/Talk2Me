265fc1a0558863b4cda551609fbafd251e519dec
Sai_Binayak <sai0binayak99@gmail.com>
2026-03-07 21:28:03 +0530

    auth routes / controllers set

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 // const express = require("express")  // loading express libraries (commonJS -> package.json("type":"commonJS"))
 import express from "express"   // express library (ES module -> package.json("type":"module"))
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express()   // creating an instance of express application
 
-
+app.use("/api/auth", authRoutes)   // adds middleware/route handlers to express app
 
 
 // server starts here
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,19 @@
+import express from "express"
+import { login, logout, signup } from "../controllers/auth.controllers.js"
+
+const router = express.Router()     // instance of router
+
+router.post("/signup", signup)
+
+router.post("/login", login)
+
+router.post("/logout", logout)
+
+export default router
+
+
+
+
+
+
+// all the logic are in controllers, to simplify routes
